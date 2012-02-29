e27f5b3ac6e7db3cbfb77fcd3e95a0858bcc8745
ekes <ekes@iskra.net>
2012-02-29 20:33:33 +0100

    Moved old 6.x-1.x attempts to branch.

--- modules/contrib/openlayers/modules/openlayers_views/views/openlayers-views-map.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: openlayers-views-map.tpl.php,v 1.1.2.3 2010/03/22 23:55:10 zzolo Exp $
-
-/**
- * @file
- * Template file for map
- */
-?>
-<div class='openlayers-views-map'><?php print $rows ?></div>

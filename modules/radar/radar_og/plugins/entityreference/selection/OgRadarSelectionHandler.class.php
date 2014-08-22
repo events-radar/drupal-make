ab9c91124365f769e7288244428fa20aa3b4534f
ekes <ekes@iskra.net>
2014-08-22 20:56:49 +0200

    Fixes (see caveat below) referencable entities for posting into, or requesting membership of.
    It makes a sql query that may become too large. See note in code
    about how to restart making the list of referencable entities and
    searching them.

--- modules/radar/radar_og/plugins/entityreference/selection/OgRadarSelectionHandler.class.php
+++ modules/radar/radar_og/plugins/entityreference/selection/OgRadarSelectionHandler.class.php
@@ -42,14 +42,23 @@ class OgRadarSelectionHandler extends OgSelectionHandler {
     $query->fieldCondition(OG_GROUP_FIELD, 'value', 1, '=');
 
     if (empty($this->instance['field_mode'])) {
-      $field_mode = 'default';
+      return $query;
     }
-    else {
-      $field_mode = $this->instance['field_mode'];
-    }
+
+    // If this is a request field then show the og that the user can't post into.
+    // This is the same behaviour as the admin field. Which makes the admin version
+    // of the field suck a bit, but first use case covered.
+    $field_mode = $this->instance['field_mode'];
+    $field_mode = $this->field['settings']['handler_settings']['membership_type'] == 'og_group_request' ? 'admin' : $field_mode;
+
     $user_groups = og_get_groups_by_user(NULL, $group_type);
     $user_groups = $user_groups ? $user_groups : array();
     $user_groups = array_merge($user_groups, $this->getGidsForCreate());
+
+    // SQL for groups that a non-member can post into:-
+    // select gid from og_role_permission inner join og_role on og_role.rid =
+    // og_role_permission.rid where og_role_permission.permission = 'create
+    // $bundle_type content' AND og_role.name = 'non-member';
 
     // Show the user only the groups they belong to.
     if ($field_mode == 'default') {
@@ -102,151 +111,26 @@ class OgRadarSelectionHandler extends OgSelectionHandler {
         $query->propertyCondition($entity_info['entity keys']['id'], $user_groups, 'NOT IN');
       }
     }
-
     return $query;
   }
-
-
-  /**
-   * Implements EntityReferenceHandler::getReferencableEntities().
-   */
-  public function getReferencableEntities($match = NULL, $match_operator = 'CONTAINS', $limit = 0) {
-    // First retrieve groups that match the search.
-    $options = array();
-    $entity_type = $this->field['settings']['target_type'];
-    $create_bundle = $this->field['instance']['bundle'];
-
-    $query = $this->buildEntityFieldQuery($match, $match_operator);
-    if ($limit > 0) {
-      $query->range(0, $limit);
-    }
-
-    $results = $query->execute();
-
-    $create_permission = 'create ' . $create_bundle . ' content'; // @todo always content?
-    $query = db_select('og_role_permission', 'p');
-    $query->join('og_role', 'r', 'r.rid = p.rid');
-    $query->addField('r', 'gid');
-    $query->condition('r.group_type', $entity_type);
-    $query->condition('r.name', 'non-member');
-    $query->condition('p.permission', $create_permission);
-    $nonmember_posting = $query->execute();
-    foreach ($nonmember_posting as $group) {
-      $results[$entity_type][$group->gid] = $group->gid;
-    }
-    if (!empty($results[$entity_type])) {
-      $entities = entity_load($entity_type, array_keys($results[$entity_type]));
-      foreach ($entities as $entity_id => $entity) {
-        list(,, $bundle) = entity_extract_ids($entity_type, $entity);
-        $options[$bundle][$entity_id] = check_plain($this->getLabel($entity));
-      }
-    }
-    return $options;
-  }
-
-  public function validateReferencableEntities(array $ids) {
-    if ($ids) {
-      $by_membership = parent::validateReferencableEntities($ids);
-
-      // and by permission.
-      $entity_type = $this->field['settings']['target_type'];
-      list(,, $create_bundle) = entity_extract_ids($this->entity_type, $this->entity);
-      $create_permission = 'create ' . $create_bundle . ' content'; // @todo always content?
-      $query = db_select('og_role_permission', 'p');
-      $query->join('og_role', 'r', 'r.rid = p.rid');
-      $query->addField('r', 'gid');
-      $query->condition('r.group_type', $entity_type);
-      $query->condition('r.name', 'non-member');
-      $query->condition('p.permission', $create_permission);
-      $query->condition('r.gid', $ids, 'IN');
-      $nonmember_posting = $query->execute();
-
-      $groups = array();
-      foreach ($nonmember_posting as $group) {
-        $groups[] = $group->gid;
-      }
-
-      $groups = array_merge($groups, $by_membership);
-
-      return $groups;
-    }
-
-    return array();
-  }
-
-  /**
-   * Overrides OgSelectionHandler::buildEntityFieldQuery().
-   *
-  public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
-    $group_type = $this->field['settings']['target_type'];
-
-    if (empty($this->instance['field_mode']) || $group_type != 'node' || user_is_anonymous()) {
-      return parent::buildEntityFieldQuery($match, $match_operator);
-    }
-
-    $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
-    $query = $handler->buildEntityFieldQuery($match, $match_operator);
-
-    // Show only the entities that are active groups.
-    $query->fieldCondition(OG_GROUP_FIELD, 'value', 1);
-    $query->fieldCondition('field_og_subscribe_settings', 'value', 'anyone');
-
-    // Add this property to make sure we will have the {node} table later on in
-    // OgRadarSelectionHandler::entityFieldQueryAlter().
-    $query->propertyCondition('nid', 0, '>');
-
-    $query->addMetaData('entityreference_selection_handler', $this);
-
-    // FIXME: http://drupal.org/node/1325628
-    unset($query->tags['node_access']);
-
-    $query->addTag('entity_field_access');
-    $query->addTag('og');
-
-    return $query;
-  }
-
-  /**
-   * Overrides OgSelectionHandler::entityFieldQueryAlter().
-   *
-   * Add the user's groups along with the rest of the "public" groups.
-   *
-  public function entityFieldQueryAlter(SelectQueryInterface $query) {
-    $gids = og_get_entity_groups();
-    if (empty($gids['node'])) {
-      return;
-    }
-
-    $conditions = &$query->conditions();
-    // Find the condition for the "field_data_field_privacy_settings" query, and
-    // the one for the "node.nid", so we can later db_or() them.
-    $public_condition = array();
-    foreach ($conditions as $key => $condition) {
-      if ($key !== '#conjunction' && is_string($condition['field'])) {
-        if (strpos($condition['field'], 'field_data_field_og_subscribe_settings') === 0) {
-          $public_condition = $condition;
-          unset($conditions[$key]);
-        }
-
-        if ($condition['field'] === 'node.nid') {
-          unset($conditions[$key]);
-        }
-      }
-    }
-
-    if (!$public_condition) {
-      return;
-    }
-
-    $or = db_or();
-    $or->condition($public_condition['field'], $public_condition['value'], $public_condition['operator']);
-    $or->condition('node.nid', $gids['node'], 'IN');
-    $query->condition($or);
-  }
-  */
 
   /**
    * Get group IDs from URL or OG-context, with access to create group-content.
+   *
+   * The query is already requiring an IN() to include groups that a user is a
+   * member of. We also require groups the user is not a member of but that
+   * allow posting by non-members.
+   *
+   * At some point this IN() is going to be too large. It would be a simple
+   * join, but for the requirement of adding the groups that the user is a
+   * member of. Phh... and then it doesn't say they can post to them.
+   * Getting the values out of audience fields on a user though is another
+   * query, or some interesting entity field query joinage?
+   *
+   * SOLUTION when this BREAKS because the query is TOO LARGE:
+   * override getReferencableEntities and build own query from scratch,
+   * at the same time countReferencableEntities and
+   * validateReferencableEntities need to use the same query (results).
    *
    * @return
    *   Array with group IDs a user (member or non-member) is allowed to
@@ -257,25 +141,13 @@ class OgRadarSelectionHandler extends OgSelectionHandler {
       return array();
     }
 
-    if (!empty($this->entity->nid)) {
-      // Existing node.
-      return array();
-    }
-
-    if (!module_exists('entityreference_prepopulate') || empty($this->instance['settings']['behaviors']['prepopulate'])) {
-      return array();
-    }
-
-    // Don't try to validate the IDs.
-    if (!$ids = entityreference_prepopulate_get_values($this->field, $this->instance, FALSE)) {
-      return array();
-    }
     $node_type = $this->instance['bundle'];
-    foreach ($ids as $delta => $id) {
-      if (!is_numeric($id) || !$id || !og_user_access($this->field['settings']['target_type'], $id, "create $node_type content")) {
-        unset($ids[$delta]);
-      }
-    }
+
+    $query = db_query(
+      'SELECT gid FROM {og_role_permission} p INNER JOIN {og_role} r ON r.rid = p.rid where p.permission = :permission AND r.name = :role_name',
+      array(':permission' => "create $node_type content", 'role_name' => 'non-member'));
+    $ids = $query->fetchCol();
     return $ids;
   }
+
 }
